c3d254d95c99593b7f53c908d0f552dcc3f7fa8e
jonz94 <jody16888@gmail.com>
2022-12-25 20:45:15 +0800

    build(deps): add `capacitor`

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.jonz94.app.phonecalldemo',
+  appName: 'Phone Call Demo',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
